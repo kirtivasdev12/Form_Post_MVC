3a18e85d9a1985b99b4d565554ce5b4a66728bd5
Kirti Vasdev <kirtivasdev12@gmail.com>
2020-07-03 01:12:16 -0400

    Add project files.

---
+++ Form_Post_MVC/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Form_Post_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Form_Post_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(PersonModel person)
+        {
+            int personId = person.PersonId;
+            string name = person.Name;
+            string gender = person.Gender;
+            string city = person.City;
+
+            return View();
+        }
+    }
+}
